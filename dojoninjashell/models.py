a6a9f20d71c5cb2b3f5740c85515f1fe1de47d7a
mohammadbisamil <m.basim.ismail@gmail.com>
2022-09-27 12:34:58 +0300

    final_sub

--- dojoninjashell/models.py
+++ dojoninjashell/models.py
@@ -6,7 +6,11 @@ class Dojo(models.Model):
     name = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     state = models.CharField(max_length=2)
+    desc = models.TextField(default="old dojo")
+
+
 # ninjas = []
+
 
 class Ninja(models.Model):
     first_name = models.CharField(max_length=255)
